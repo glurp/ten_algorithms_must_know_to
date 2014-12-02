a388f09b99218f57d1a517e706aa8bb9c9f57aad
unknown <rdaubarede@.(none)>
2014-12-02 19:26:39 +0100

    creation

---
+++ ten_test.rb
@@ -0,0 +1,20 @@
+require "benchmark"
+
+def ten_test
+  trace=(ARGV.first=="-d" && ARGV.shift)
+  filter=ARGV.shift
+
+  lmethods = private_methods.grep(/^test_/)
+  puts "Methods testable:  #{lmethods.map(&:to_s).join(" ")}"
+  puts "Methods to test :  #{lmethods.map(&:to_s).grep(/#{filter}/).join(" ")}" if filter
+
+  if trace 
+    lmethods.each { |met| (puts "#{met}:" ; send(met,true); puts)  if !filter || met =~ /#{filter}/ }
+  end
+
+  Benchmark.bm { |x|
+    lmethods.each { |met|
+      x.report(met) { send(met,false) } if !filter || met =~ /#{filter}/
+    }
+  }
+end
