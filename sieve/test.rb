e87d334dd9882b4eaec09232bafdb9c5af7a0683
unknown <rdaubarede@.(none)>
2014-12-02 19:26:33 +0100

    creation

--- sieve/test.rb
+++ sieve/test.rb
@@ -1,19 +1,4 @@
-require 'benchmark'
+require_relative '../ten_test.rb'
 require_relative 'simples.rb'
 
-trace=(ARGV.first=="-d" && ARGV.shift)
-filter=ARGV.shift
-
-lmethods = private_methods.grep(/^test_/)
-puts "Methods testable:  #{lmethods.map(&:to_s).join(" ")}"
-puts "Methods to test :  #{lmethods.map(&:to_s).grep(/#{filter}/).join(" ")}" if filter
-
-if trace 
-  lmethods.each { |met| (puts "#{met}:" ; send(met,true); puts)  if !filter || met =~ /#{filter}/ }
-end
-
-Benchmark.bm { |x|
-  lmethods.each { |met|
-    x.report(met) { send(met,false) } if !filter || met =~ /#{filter}/
-  }
-}
+ten_test
